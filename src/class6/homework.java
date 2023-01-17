211095a55b607cd15c9c0cd1592f5cf07bd12be0
alper bobaj <alperbobaj@alpers-MacBook-Pro-2.local>
2023-01-17 18:08:07 -0500

    array HomeWork

--- src/class6/homework.java
+++ src/class6/homework.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class homework {
   public static void main(String[] args) {
 
-    int number = 10;
-    while (number <=19) {
-      System.out.println(number);
-      number ++;
-    }
+
+  for (int i=2; i<10; ){
+
+  }
+
   }
 }
---
+++ src/homework1/Task3.java
@@ -0,0 +1,20 @@
+package homework1;
+
+public class Task3 {
+
+    public static void main(String[] args) {
+        String[] day = {"Saturday", "is", "Java", "coding", "Day."};
+
+
+        for (int i = 0; i < day.length ; i++) {
+            System.out.print(day[i]+" ");
+
+        }
+        System.out.println();
+
+
+        String[]days={"Java","Saturday","day","coding","is"};
+        System.out.println(days[1]+" "+days[4]+" "+days[0]+" "+days[3]+" "+days[2]+".");
+
+    }
+}
